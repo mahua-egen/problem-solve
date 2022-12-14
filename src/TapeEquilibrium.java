f6cc2cada7f5be711efa0a24992dc192e00fd4ea
mahua <mahua.majid@egeneration.co>
2022-12-14 14:29:23 +0600

    solve problem TapeEquilibrium

--- src/TapeEquilibrium.java
+++ src/TapeEquilibrium.java
@@ -1,10 +1,25 @@
 public class TapeEquilibrium {
     public static void main(String[] args) {
-        int A[] = {2, 3, 1, 5};
-        TapeEquilibrium();
+        int A[] = {3, 1, 2, 4, 3};
+        System.out.println(TapeEquilibrium(A));
     }
 
-    public static int TapeEquilibrium() {
-        return 0;
+    public static int TapeEquilibrium(int A[]) {
+        int len = A.length;
+        double div = Integer.MAX_VALUE;
+        double[] dd = new double[len];
+        dd[0] = A[0];
+        for (int i = 1; i < len; i++) {
+            dd[i] = dd[i - 1] + A[i];
+        }
+        double temp = dd[len - 1];
+        for (int i = 0; i < len - 1; i++) {
+            temp = temp - A[i];
+            double abs = Math.abs(dd[i] - temp);
+            if (div > abs) {
+                div = abs;
+            }
+        }
+        return (int) div;
     }
 }
