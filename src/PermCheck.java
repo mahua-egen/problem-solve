83ef5824701a1c200a490026d45f59515f25a756
mahua <mahua.majid@egeneration.co>
2022-12-21 10:40:44 +0600

    solve problem PermCheck

--- src/PermCheck.java
+++ src/PermCheck.java
@@ -3,19 +3,21 @@ import java.util.Set;
 
 public class PermCheck {
     public static void main(String[] args) {
-        int[] A = {4, 1, 3, 2};
+        double[] A = {4, 1, 3, 2};
         System.out.println(permCheck(A));
     }
 
-    public static int permCheck(int[] A) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 1; i < A.length; i++) {
-            if (!set.add(A[i])) {
+    public static int permCheck(double[] A) {
+        Set<Double> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            double j = A[i];
+            if (!set.add(j)) {
                 return 0;
             }
         }
-        for (int i = 1; i < A.length; i++) {
-            if (!set.contains(i)) {
+        for (int i = 0; i < A.length; i++) {
+            double j = i + 1;
+            if (!set.contains(j)) {
                 return 0;
             }
         }
